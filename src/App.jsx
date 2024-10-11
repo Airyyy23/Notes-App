94e69ec4751dc5205e05bf883f47658a211d8f5d
Airyyy23 <wayangseno123@gmail.com>
2024-10-11 08:05:10 +0700

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,24 @@ const MainComponent = () => {
   const { toggleShowForm, showForm } = useContext(NoteContext);
 
   return (
-    <div>
-      <h1 className="text-center text-4xl max-md:text-2xl font-bold my-5">
-        Personal Note Apps
-      </h1>
-      <hr />
-      <NoteForm />
-      <hr className={showForm ? 'block w-4/5 m-auto my-5' : 'hidden'} />
-      <div className="flex justify-center my-5">
-        <CustomButton
-          content={showForm ? 'Close' : 'Add Note'}
-          onClick={toggleShowForm}
-        />
+    <div className='h-screen flex flex-col justify-between'>
+      <div>
+        <h1 className="text-center text-4xl max-md:text-2xl font-bold my-5">
+          Personal Note Apps
+        </h1>
+        <hr />
+        <NoteForm />
+        <hr className={showForm ? 'block w-4/5 m-auto my-5' : 'hidden'} />
+        <div className="flex justify-center my-5">
+          <CustomButton
+            content={showForm ? 'Close' : 'Add Note'}
+            onClick={toggleShowForm}
+          />
+        </div>
+        <hr className="w-4/5 m-auto mb-5" />
+        <SearchBar />
+        <NoteList />
       </div>
-      <hr className="w-4/5 m-auto mb-5" />
-      <SearchBar />
-      <NoteList />
       <Footer />
     </div>
   );
